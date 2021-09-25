144b3df10bfb9dedcc8982bfe698f638062ca505
Harshith Gowda <harshithgowdakt@gmail.com>
2021-09-25 18:13:54 +0530

    changed to echo module

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,55 @@
 package main
 
 import (
-	"io"
-	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
-func main()  {
-	helloHandler := func (w http.ResponseWriter, r *http.Request)  {
-		io.WriteString(w, "hellow, world")
-	}
+type user struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
 
-	http.HandleFunc("/hello", helloHandler)
-	log.Println("Listening to request at http://localhost:8000/hello")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+var (
+	users = map[int]*user{}
+	seq   = 1
+)
+
+func creatUsers(c echo.Context) error {
+	u := &user{
+		ID: seq,
+	}
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	users[u.ID] = u
+	seq++
+	return c.JSON(http.StatusCreated, u)
+}
+
+func getUser(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	return c.JSON(http.StatusOK, users[id])
+}
+
+func deleteUser(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	delete(users, id)
+	return c.NoContent(http.StatusNoContent)
+}
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	//Routes
+	e.POST("/api/v1/users", creatUsers)
+	e.GET("/api/v1/users/:id", getUser)
+	e.DELETE("/api/v1/users/:id", deleteUser)
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
